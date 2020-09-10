432327b6e162ef6638cdf837b2ec44e2c280fbaf
akishinoshiame <akishinoshiame@icloud.com>
2020-09-10 12:05:07 +0800

    Content Update.

--- SmoothScroll/Smooth.js
+++ SmoothScroll/Smooth.js
@@ -1,16 +1,16 @@
 $(document).ready(function(){
     $('.navbar-brand').click(function(e){
         e.preventDefault();
-        var TargetToSmooth = $(this).attr('href');
+        var TargetToSmooth = $(this).attr('alt');
         $('html, body').animate({
             scrollTop: $(TargetToSmooth).offset().top
         }, 700);
     });
     $('.btn').click(function(e){
         e.preventDefault();
-        var TargetToSmooth = $(this).attr('href');
+        var TargetToSmooth = $(this).attr('alt');
         $('html, body').animate({
             scrollTop: $(TargetToSmooth).offset().top
-        }, 1000);
+        }, 500);
     });
 });
